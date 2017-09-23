1e75e5cc9e09b783729cea0bca1ac7f52b8098a7
Seong soo Park <psstjdtn1@naver.com>
2017-09-23 16:24:50 +0900

    20170923 ver1.6

--- httpExam/request.response.js
+++ httpExam/request.response.js
@@ -1,17 +1,42 @@
 var http = require('http');
 var fs = require('fs');
+var ejs = require('ejs');
+var jade = require('jade');
+
 http.createServer(function(req,res){
 	if(req.method == 'GET') {
 		console.log(req.url+" GET");
-		fs.readFile('index.html', function(err,data) {
-			if(!err) {
-				res.writeHead(200, {'Content-Type':'text/html'});
-				res.end(data);
-			} else {
-				res.writeHead(404);
-				res.end();
-			}
-		});
+		if(req.url == '/'){
+			fs.readFile('index.html', function(err,data) {
+				if(!err) {
+					res.writeHead(200, {'Content-Type':'text/html'});
+					res.end(data);
+				} else {
+					res.writeHead(404);
+					res.end();
+				}
+			});	
+		} else if (req.url == '/ejs') {
+			fs.readFile('templete.ejs', 'utf8', function(err,data){
+				if(!err) {
+					var html = ejs.render(data, 
+						{name:'Hong',description:'Hello World for EJS'});
+					res.writeHead(200, {'Content-Type':'text/html'});
+					res.end(html);
+				}
+			})
+		} else if (req.url == '/jade') {
+			fs.readFile('templete.jade', 'utf8', function(err,data){
+				if (!err) {
+					var fn = jade.compile(data);
+					var html = fn({
+						name:'PARK',description:'Hello World for JADE'
+					});
+					res.writeHead(200, {'Content-Type':'text/html'});
+					res.end(html);
+				}
+			})
+		}
 	} else if (req.method == 'POST') {
 		console.log(req.url+" POST");	
 		req.on('data', function(data){
